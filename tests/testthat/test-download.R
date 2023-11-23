ad2159c31ec5523329c575cb498db24260088c64
Selcuk Korkmaz <selcukorkmaz@hotmail.com>
2023-11-23 12:21:02 +0300

    update

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -1,22 +1,22 @@
 # Create directory ----
-test_that("create 'path' if it does not exist.", {
-  temp_dir <- tempdir(check = TRUE)
-
-  try(download(
-    outformat = "json",
-    path = temp_dir,
-    identifier = "aspirin",
-    namespace = "name",
-    domain = "compound",
-    operation = NULL,
-    searchtype = NULL,
-    overwrite = FALSE
-  ))
-
-  expect_true(file.exists(temp_dir))
-  file.remove(file.path(temp_dir, "aspirin.json"))
-  file.remove(temp_dir)
-})
+# test_that("create 'path' if it does not exist.", {
+#   temp_dir <- tempdir(check = TRUE)
+#
+#   try(download(
+#     outformat = "json",
+#     path = temp_dir,
+#     identifier = "aspirin",
+#     namespace = "name",
+#     domain = "compound",
+#     operation = NULL,
+#     searchtype = NULL,
+#     overwrite = FALSE
+#   ))
+#
+#   expect_true(file.exists(temp_dir))
+#   file.remove(file.path(temp_dir, "aspirin.json"))
+#   file.remove(temp_dir)
+# })
 
 # 'overwrite' file/folder
 test_that("overwrite file if it already exists.", {
@@ -44,14 +44,14 @@ test_that("overwrite file if it already exists.", {
       domain = "compound",
       operation = NULL,
       searchtype = NULL,
-      overwrite = FALSE
+      overwrite = TRUE
     ))
 
     mtime <- file.info(file.path(temp_dir, "aspirin.json"))$mtime
     expect_false(identical(ctime, mtime))
 
     file.remove(file.path(temp_dir, "aspirin.json"))
-    file.remove(temp_dir)
+    # file.remove(temp_dir)
   }
 })
 
@@ -84,7 +84,7 @@ test_that("return error if overwrite is FALSE and file exists.", {
   })
 
   file.remove(file.path(temp_dir, "aspirin.json"))
-  file.remove(temp_dir)
+  # file.remove(temp_dir)
 })
 
 # Download file in SDF/JSON formats ----
@@ -119,6 +119,6 @@ test_that("create 'path' if it does not exist.", {
   expect_true(all(file.exists(json_file), file.exists(sdf_file)))
   file.remove(json_file)
   file.remove(sdf_file)
-  file.remove(temp_dir)
+  # file.remove(temp_dir)
 })
 
